06ddb8edfd30baaceadf991148924c07cdf05461
qxieyong <143081419+qxieyong@users.noreply.github.com>
2024-04-18 17:09:43 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
