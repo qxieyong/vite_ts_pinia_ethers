d89246564dad313e0c30f088980ff58105c0ff49
qxieyong <143081419+qxieyong@users.noreply.github.com>
2024-04-23 16:35:56 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,80 +8,81 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    //hmr: { overlay: false },
-    https: false, // 是否开启 https
-    open: false, // 是否自动在浏览器打开
-    port: 5174, // 端口号
-    host: "0.0.0.0",
-    proxy: {
-      "/api": {
-        target: "", // 后台接口
-        changeOrigin: true,
-        secure: false, // 如果是https接口，需要配置这个参数
-        // ws: true, //websocket支持
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-    viteCompression({
-      threshold: 1024 * 1024 *5 // 对大于 5MB 的文件进行压缩
-    })
-  ],
-  build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        //生产环境时移除console
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-    rollupOptions:{
-      output:{
-        entryFileNames:"js/[name]-[hash].js",  //入口js文件存放
-        chunkFileNames:"js/[name]-[hash].js", //其他js文件存放
-        assetFileNames(assetInfo){ //其他文件的配置
-          if(assetInfo.name.endsWith('.css')){
-            return 'css/[name]-[hash].css';
-          }
-          const imgExts = ['.png','.jpg','.jpeg','webp','.svg','.gif','.ico'];
-          if(imgExts.some(ext=>assetInfo.name.endsWith(ext))){
-            return 'img/[name]-[hash].[ext]';
-          }
-          return "assets/[name]-[hash].[ext]"
+export default defineConfig(({ mode }) => {
+    return {
+        server: {
+            //hmr: { overlay: false },
+            https: false, // 是否开启 https
+            open: false, // 是否自动在浏览器打开
+            port: 5174, // 端口号
+            host: "0.0.0.0",
+            proxy: {
+                "/api": {
+                    target: "", // 后台接口
+                    changeOrigin: true,
+                    secure: false, // 如果是https接口，需要配置这个参数
+                    // ws: true, //websocket支持
+                    rewrite: (path) => path.replace(/^\/api/, ""),
+                },
+            },
         },
-        manualChunks(id){
-          console.log(id);
-          if(id.includes('node_modules')){
-            return 'vendor';
-          }
+        plugins: [
+            vue(),
+            AutoImport({
+                resolvers: [ElementPlusResolver()],
+            }),
+            Components({
+                resolvers: [ElementPlusResolver()],
+            }),
+            viteCompression({
+                threshold: 1024 * 1024 * 5 // 对大于 5MB 的文件进行压缩
+            })
+        ],
+        build: {
+            minify: 'terser',
+            terserOptions: mode === 'production' ? {
+                compress: {
+                    //生产环境时移除console
+                    drop_console: true,
+                    drop_debugger: true,
+                },
+            } : {},
+            rollupOptions: {
+                output: {
+                    entryFileNames: "js/[name]-[hash].js",  //入口js文件存放
+                    chunkFileNames: "js/[name]-[hash].js", //其他js文件存放
+                    assetFileNames(assetInfo) { //其他文件的配置
+                        if (assetInfo.name.endsWith('.css')) {
+                            return 'css/[name]-[hash].css';
+                        }
+                        const imgExts = ['.png', '.jpg', '.jpeg', 'webp', '.svg', '.gif', '.ico'];
+                        if (imgExts.some(ext => assetInfo.name.endsWith(ext))) {
+                            return 'img/[name]-[hash].[ext]';
+                        }
+                        return "assets/[name]-[hash].[ext]"
+                    },
+                    manualChunks(id) {
+                        console.log(id);
+                        if (id.includes('node_modules')) {
+                            return 'vendor';
+                        }
+                    }
+                }
+
+            }
+        },
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "src"),
+                'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js',
+            },
+        },
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: `@import '@/assets/scss';`
+                }
+            }
         }
-      }
-      
     }
-    
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-      'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js',
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import '@/assets/scss';`
-      }
-    }
-  }
 })
