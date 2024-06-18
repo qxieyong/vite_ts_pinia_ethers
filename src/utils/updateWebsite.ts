789f74ee3ec35513efb6577e234ba8f20880fe54
qxieyong <143081419+qxieyong@users.noreply.github.com>
2024-06-18 18:18:13 +0800

    Update updateWebsite.ts

--- src/utils/updateWebsite.ts
+++ src/utils/updateWebsite.ts
@@ -3,12 +3,14 @@ const updateWebsite = () => {
         .then(response => response.text())
         .then(data => {
             const dom = new DOMParser().parseFromString(data, 'text/html');
-            const scripts = dom.querySelectorAll('script');
+            const scripts = dom.querySelectorAll('script[type="module"]') as unknown as HTMLScriptElement[];
+            console.log(scripts, 'bbb')
             scripts.forEach(script => {
+                console.log(script, '标签');
                 if (script.src.includes(window.location.origin)) {
                     const currentVersion = window.localStorage.getItem('version');
                     if (currentVersion !== script.src) {
-                        console.log('版本不一样', currentVersion,script.src);
+                        console.log('版本不一样', currentVersion, script.src);
                         window.localStorage.setItem('version', script.src);
                         setTimeout(() => {
                             location.reload();
