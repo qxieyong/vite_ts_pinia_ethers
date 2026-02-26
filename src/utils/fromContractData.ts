b7f6b0fcc5bb2e2f91a145f8fa55b25640a2478b
xieyong <1239665882@qq.com>
2026-02-26 17:29:16 +0800

    删除 SwitchUtils.ts 和 thhersUtils.ts 文件，更新 fromContractData.ts 中的 ethers 方法调用

--- src/utils/fromContractData.ts
+++ src/utils/fromContractData.ts
@@ -4,14 +4,14 @@ import { ethers } from "ethers";
  * @param {number|string} val 给合约需要转换的值
  */
 const parseUnits = (val: number | string) => {
-	return ethers.utils.parseUnits(String(val), 18);
+	return ethers.parseUnits(String(val), 18);
 };
 
 /**
  * @param {number} val 从合约上拿下来需要转换的值
  */
 const formatUnits = (val: number) => {
-	return ethers.utils.formatUnits(val, 18);
+	return ethers.formatUnits(val, 18);
 };
 
 export { parseUnits, formatUnits };
