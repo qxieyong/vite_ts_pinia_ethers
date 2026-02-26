27c55d63d0e4f6cdf764145e12074e38044d5054
xieyong <1239665882@qq.com>
2026-02-26 17:29:11 +0800

    移除 useWalletListener 钩子并简化 App.vue 代码结构，添加 .gitkeep 文件以保持 hooks 目录

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <script setup lang="ts">
 import { provide } from "vue";
 
-import { useWalletListener } from "@/hooks/useWalletListener";
-
 const cast3 = new BroadcastChannel("channel3");
 provide("cast3", cast3);
-
-useWalletListener();
 </script>
 
 <template>
